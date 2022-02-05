dfb54476fed16a04dbfe26520100b9f8026169eb
cabreratox <mcabrera1001@gmail.com>
2022-02-05 09:27:33 -0500

    working on installing all R dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM rocker/r-ver:3.2
 ADD * /app/
 
-RUN apt-get update && apt-get install python-dev python-pip dirmngr apt-transport-https ca-certificates software-properties-common gnupg2 libcurl4-openssl-dev libssl-dev -y
+RUN apt-get update && apt-get install python-dev python-pip dirmngr \
+apt-transport-https ca-certificates software-properties-common gnupg2 libcurl4-openssl-dev libssl-dev libnlopt-dev -y
 RUN pip install -r /app/requirements.txt
 # RUN apt-get install dirmngr apt-transport-https ca-certificates software-properties-common gnupg2 -y
--- packages.R
+++ packages.R
@@ -1,18 +1,24 @@
 install.packages("devtools")
 require(devtools)
 
-# install_version("argparse", version="1.0.1")
-packages <- list("plyr" = "1.8.3", "gbm" = "2.1.1", "survival" = "2.38-3", "kernlab" = "0.9-22", "argparse" = "1.0.1", "proto" = "0.3-10", "caret" = "6.0-64", "ggplot2" = "2.0.0", "lattice" = "0.20-33")
+# # install_version("argparse", version="1.0.1")
+packages <- list("plyr" = "1.8.3", "gbm" = "2.1.1", "survival" = "2.38-3", "kernlab" = "0.9-22", "argparse" = "1.0.1", "proto" = "0.3-10", "ggplot2" = "2.0.0", "lattice" = "0.20-33")
+# # packages <- list("caret" = "6.0-90")
 
-# for (p in packages) {
-#     print(p)
+# # for (p in packages) {
+# #     print(p)
+# # }
+# # print(names(packages))
+
+# # print(paste(packages, names(packages)))
+
+# for (name in names(packages)) {
+#     # print(packages[[name]])
+#     # print(name)
+#     install_version(name, version=packages[[name]])
 # }
-# print(names(packages))
 
-# print(paste(packages, names(packages)))
-
-for (name in names(packages)) {
-    # print(packages[[name]])
-    # print(name)
-    install_version(name, version=packages[[name]])
-}
+# TODO: Struggling to install caret, a dependency issue
+install.packages("caret")
+library(caret)
+sessionInfo()
